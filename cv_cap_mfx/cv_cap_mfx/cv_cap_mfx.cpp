f59370728a4430498ebcaf44f3d60a49fb98a80b
kurekagura <kurekagura@gmail.com>
2022-01-07 12:27:14 +0900

    Encode/Decode(H.264) video using CAP_INTEL_MFX in OpenCV.

---
+++ cv_cap_mfx/cv_cap_mfx/cv_cap_mfx.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <opencv2\opencv.hpp>
+
+int main()
+{
+	//cv::VideoCapture capture("C:\\dev\\samplevideo\\Big_Buck_Bunny_360_10s_1MB.mp4", cv::CAP_FFMPEG);
+	//CAP_INTEL_MFXは、mp4などのコンテナには未対応（raw(.264)のみに対応）。
+	cv::VideoCapture capture("C:\\dev\\samplevideo\\test.264", cv::CAP_INTEL_MFX);
+
+	//cv::VideoCapture capture(0, cv::CAP_MSMF); //OK
+	//cv::VideoCapture capture(0, cv::CAP_DSHOW); //OK
+	//cv::VideoCapture capture(0); //OK 既定がおそらくCAP_MSMFかCAP_DSHOW
+
+	//cv::VideoCapture capture("C:\\dev\\samplevideo\\Big_Buck_Bunny_360_10s_1MB.mp4", cv::CAP_INTEL_MFX);
+	//->MFX: Unsupported extension: C:\dev\samplevideo\Big_Buck_Bunny_360_10s_1MB.mp4
+
+	int width = (int)capture.get(cv::CAP_PROP_FRAME_WIDTH);
+	int height = (int)capture.get(cv::CAP_PROP_FRAME_HEIGHT);
+	int count = (int)capture.get(cv::CAP_PROP_FRAME_COUNT);
+	int fourcc = (int)capture.get(cv::CAP_PROP_FOURCC);
+	double fps = capture.get(cv::CAP_PROP_FPS);
+
+	//cv::VideoWriter writer("out.avi", cv::CAP_FFMPEG, fourcc, fps, cv::Size(width, height), false);
+	//未指定はCAP_FFMPEG(->openh264-1.8.0-win64.dllが必要)
+
+	int out_fourcc = cv::VideoWriter::fourcc('H', '2', '6', '4');
+	double out_fps = 30;
+	//cv::VideoWriter writer("out_CAP_FFMPEG.mp4", cv::CAP_FFMPEG, out_fourcc, fps, cv::Size(width, height), true);
+
+	cv::VideoWriter writer("C:\\dev\\samplevideo\\out_CAP_INTEL_MFX.264", cv::CAP_INTEL_MFX, out_fourcc, out_fps, cv::Size(width, height), true);
+
+	if (!writer.isOpened())
+		std::cout << "* Failed to open VideoWriter" << std::endl;
+
+	while (true)
+	{
+		cv::Mat frame;
+		capture >> frame;
+		if (frame.empty())
+			break;
+
+		cv::imshow("showing", frame);
+
+		if (cv::waitKey(1) == 'q')
+			break;
+
+		if (writer.isOpened())
+			writer << frame;
+	}
+
+	return 0;
+}
