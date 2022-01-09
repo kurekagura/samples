ade39366e027ae4404af4223ab72af90d6acbf3c
kurekagura <kurekagura@gmail.com>
2022-01-09 21:18:25 +0900

    Encode using CAP_GSTREAMER of cv.

--- cv_cap_gst/cv_cap_gst/cv_cap_gst.cpp
+++ cv_cap_gst/cv_cap_gst/cv_cap_gst.cpp
@@ -1,6 +1,26 @@
 ﻿#include <iostream>
 #include <opencv2\opencv.hpp>
 
+void play_to_validate(const cv::String outputPath) {
+
+	cv::String gst_cmd_cap("filesrc location=\"");
+	gst_cmd_cap += outputPath;
+	gst_cmd_cap += "\" !h264parse !d3d11h264device1dec !videoconvert !appsink";
+	cv::VideoCapture capture(gst_cmd_cap, cv::CAP_GSTREAMER);
+
+	while (true)
+	{
+		cv::Mat cvmat;
+		if (!capture.read(cvmat))
+			break;
+
+		cv::imshow("Validating:" + outputPath, cvmat);
+
+		if (cv::waitKey(1) == 'q')
+			break;
+	}
+}
+
 int main()
 {
 	std::cout << cv::getBuildInformation() << std::endl;
@@ -20,16 +40,39 @@ int main()
 	int fourcc = (int)capture.get(cv::CAP_PROP_FOURCC);	//?
 	double fps = capture.get(cv::CAP_PROP_FPS);	//ok
 
+	int out_width = width;
+	int out_height = height;
+	int out_fourcc = cv::VideoWriter::fourcc('H', '2', '6', '4');
+	double out_fps = fps;
+
+	const cv::String outputPath("C:\\\\dev\\\\samplevideo\\\\out-cv_cap_gst.264");
+	cv::String gst_cmd_writer("appsrc !videoconvert !mfh264device1enc !filesink location=\"");
+	gst_cmd_writer += outputPath;
+	gst_cmd_writer += "\"";
+	cv::VideoWriter writer(gst_cmd_writer, cv::CAP_GSTREAMER, out_fourcc, out_fps, cv::Size(width, height), true);
+	
+	if (!writer.isOpened())
+		std::cerr << "Error : to open VideoWriter(CAP_GSTREAMER)" << std::endl;
+
+	const cv::String windowName("window1 - cv_cap_gst");
 	while (true)
 	{
 		cv::Mat cvmat;
 		if (!capture.read(cvmat))
 			break;
 
-		cv::imshow("MyWindow1", cvmat);
+		cv::imshow(windowName, cvmat);
 
 		if (cv::waitKey(1) == 'q')
 			break;
+
+		if (writer.isOpened())
+			writer.write(cvmat);
 	}
+
+	cv::destroyWindow(windowName);
+
+	play_to_validate(outputPath);
+
 	return 0;
 }
