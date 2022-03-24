d46ea80fa78903a457536c5032501afd2a6cb034
kurekagura <kurekagura@gmail.com>
2022-03-25 00:12:38 +0900

    Changed to be able to use OpenCV.

--- qt/QtCreator4CMakeBehavior/myapp/mainwindow.cpp
+++ qt/QtCreator4CMakeBehavior/myapp/mainwindow.cpp
@@ -1,11 +1,16 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <opencv2/opencv.hpp>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    cv::Mat mat = cv::imread("C:/Windows/Web/Wallpaper/Theme1/img1.jpg");
+    if(mat.empty())
+        return;
 }
 
 MainWindow::~MainWindow()
