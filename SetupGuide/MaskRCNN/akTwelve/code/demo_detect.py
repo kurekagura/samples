daa986bbc530b918ca783d9df08e815adaf7546d
kurekagura <kurekagura@gmail.com>
2022-11-07 22:26:51 +0900

    リファクタリング

--- SetupGuide/MaskRCNN/akTwelve/code/demo_detect.py
+++ SetupGuide/MaskRCNN/akTwelve/code/demo_detect.py
@@ -1,202 +1,72 @@
 """
-refs:
-https://github.com/akTwelve/Mask_RCNN/blob/master/samples/demo.ipynb
-https://github.com/taisuke-ito/samples2/blob/master/demo_Analyze.ipynb
+Modified: https://github.com/akTwelve/Mask_RCNN/blob/master/samples/demo.ipynb
 """
-import matplotlib
-
 import os
 import sys
 import random
-import math
-import numpy as np
-import skimage.io
+import matplotlib
 import matplotlib.pyplot as plt
-from matplotlib import patches,  lines
-from skimage.measure import find_contours
-from matplotlib.patches import Polygon
-import mrcnn.model as modellib
-from mrcnn import utils
-from mrcnn import visualize
-import colorsys
+import mrcnn.model as mrcnn_model
+import mrcnn.visualize as mrcnn_viz
+import skimage.io as skio
 
-def apply_mask(image, mask, color, alpha=0.5):
-    """Apply the given mask to the image.
-    """
-    for c in range(3):
-        image[:, :, c] = np.where(mask == 1, image[:, :, c] * (1 - alpha) + alpha * color[c] * 255, image[:, :, c])
-    return image
+ROOT_DIR = os.path.abspath("./")
 
-def random_colors(N, bright=True):
-    """
-    Generate random colors.
-    To get visually distinct colors, generate them in HSV space then
-    convert to RGB.
-    """
-    brightness = 1.0 if bright else 0.7
-    hsv = [(i / N, 1, brightness) for i in range(N)]
-    colors = list(map(lambda c: colorsys.hsv_to_rgb(*c), hsv))
-    random.shuffle(colors)
-    return colors
+sys.path.append(ROOT_DIR)  # To find local version of the library
+sys.path.append(os.path.join(ROOT_DIR, "samples/coco/"))  # To import local coco config.
+print(f"=======================>{matplotlib.get_backend()}")
+import coco
+print(f"=======================>{matplotlib.get_backend()}")
+class InferenceConfig(coco.CocoConfig):
+    # Set batch size to 1 since we'll be running inference on
+    # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
 
-def display_instances(image, boxes, masks, class_ids, class_names, scores=None, title="", figsize=(16, 16), ax=None, show_mask=True, show_bbox=True, colors=None, captions=None):
-    """
-    boxes: [num_instance, (y1, x1, y2, x2, class_id)] in image coordinates.
-    masks: [height, width, num_instances]
-    class_ids: [num_instances]
-    class_names: list of class names of the dataset
-    scores: (optional) confidence scores for each box
-    title: (optional) Figure title
-    show_mask, show_bbox: To show masks and bounding boxes or not
-    figsize: (optional) the size of the image
-    colors: (optional) An array or colors to use with each object
-    captions: (optional) A list of strings to use as captions for each object
-    """
-    # Number of instances
-    N = boxes.shape[0]
+# COCO Class names
+# For example, to get ID of　the teddy bear class, use: class_names.index('teddy bear')
+class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+                'bus', 'train', 'truck', 'boat', 'traffic light',
+                'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
+                'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
+                'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
+                'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+                'kite', 'baseball bat', 'baseball glove', 'skateboard',
+                'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+                'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+                'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+                'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
+                'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
+                'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
+                'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
+                'teddy bear', 'hair drier', 'toothbrush']
 
-    # If no axis is passed, create one and automatically call show()
-    auto_show = False
-    if not ax:
-        _, ax = plt.subplots(1, figsize=figsize)
-        auto_show = True
-
-    # Generate random colors
-    colors = colors or random_colors(N)
-
-    # Show area outside image boundaries.
-    height, width = image.shape[:2]
-    ax.set_ylim(height + 10, -10)
-    ax.set_xlim(-10, width + 10)
-    ax.axis('off')
-    ax.set_title(title)
-
-    masked_image = image.astype(np.uint32).copy()
-    for i in range(N):
-        color = colors[i]
-
-        # Bounding box
-        if not np.any(boxes[i]):
-            # Skip this instance. Has no bbox. Likely lost in image cropping.
-            continue
-        y1, x1, y2, x2 = boxes[i]
-        if show_bbox:
-            p = patches.Rectangle((x1, y1), x2 - x1, y2 - y1, linewidth=2, alpha=0.7, linestyle="dashed", edgecolor=color, facecolor='none')
-            ax.add_patch(p)
-
-        # Label
-        if not captions:
-            class_id = class_ids[i]
-            score = scores[i] if scores is not None else None
-            label = class_names[class_id]
-            caption = "{} {:.3f}".format(label, score) if score else label
-        else:
-            caption = captions[i]
-        ax.text(x1, y1 + 8, caption, color='w', size=11, backgroundcolor="none")
-
-        # Mask
-        mask = masks[:, :, i]
-        if show_mask:
-            masked_image = apply_mask(masked_image, mask, color)
-
-        # Mask Polygon
-        # Pad to ensure proper polygons for masks that touch image edges.
-        padded_mask = np.zeros((mask.shape[0] + 2, mask.shape[1] + 2), dtype=np.uint8)
-        padded_mask[1:-1, 1:-1] = mask
-        contours = find_contours(padded_mask, 0.5)
-        for verts in contours:
-            # Subtract the padding and flip (y, x) to (x, y)
-            verts = np.fliplr(verts) - 1
-            p = Polygon(verts, facecolor="none", edgecolor=color)
-            ax.add_patch(p)
-    ax.imshow(masked_image.astype(np.uint8))
-    if auto_show:
-        plt.show()
-
-def detect():
-    # Root directory of the project
-    ROOT_DIR = os.path.abspath("./")
-
-    print(f"=======================>{matplotlib.get_backend()}")
-
-    # Import Mask RCNN
-    sys.path.append(ROOT_DIR)  # To find local version of the library
-    # Import COCO config
-    sys.path.append(os.path.join(ROOT_DIR, "samples/coco/"))  # To find local version
-    import coco
-    print(f"=======================>{matplotlib.get_backend()}")
-    matplotlib.use('TkAgg')
-    print(f"=======================>{matplotlib.get_backend()}")
-
-    class InferenceConfig(coco.CocoConfig):
-        # Set batch size to 1 since we'll be running inference on
-        # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
-        GPU_COUNT = 1
-        IMAGES_PER_GPU = 1
-
-    print(f"=======================>{matplotlib.get_backend()}")
-
-    # Directory to save logs and trained model
-    MODEL_DIR = os.path.join(ROOT_DIR, "logs")
-
-    # Local path to trained weights file
-    COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
-    # Download COCO trained weights from Releases if needed
-    if not os.path.exists(COCO_MODEL_PATH):
-        utils.download_trained_weights(COCO_MODEL_PATH)
-
-    # Directory of images to run detection on
-    IMAGE_DIR = os.path.join(ROOT_DIR, "images")
-
+def create_detector(coco_model_path):
     config = InferenceConfig()
     config.display()
+    model = mrcnn_model.MaskRCNN(mode="inference", model_dir="", config=config)
+    model.load_weights(coco_model_path, by_name=True)   # Load weights trained on MS-COCO
+    return model
 
-    # Create model object in inference mode.
-    model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
+if __name__ == "__main__":
+    matplotlib.use('TkAgg')
 
-    # Load weights trained on MS-COCO
-    model.load_weights(COCO_MODEL_PATH, by_name=True)
+    coco_model_path = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")   # Local path to trained weights file
+    if not os.path.exists(coco_model_path):
+        utils.download_trained_weights(coco_model_path)
 
-    # COCO Class names
-    # Index of the class in the list is its ID. For example, to get ID of
-    # the teddy bear class, use: class_names.index('teddy bear')
-    class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
-                   'bus', 'train', 'truck', 'boat', 'traffic light',
-                   'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
-                   'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
-                   'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
-                   'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
-                   'kite', 'baseball bat', 'baseball glove', 'skateboard',
-                   'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
-                   'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
-                   'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
-                   'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
-                   'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
-                   'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
-                   'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
-                   'teddy bear', 'hair drier', 'toothbrush']
+    image_dir = os.path.join(ROOT_DIR, "images")    # Directory of images to run detection on
+    file_names = next(os.walk(image_dir))[2]        # Load a random image from the images folder
+    image = skio.imread(os.path.join(image_dir, random.choice(file_names)))
 
-    # Load a random image from the images folder
-    file_names = next(os.walk(IMAGE_DIR))[2]
-    image = skimage.io.imread(os.path.join(IMAGE_DIR, random.choice(file_names)))
+    detector = create_detector(coco_model_path)
 
-    # Run detection
-    results = model.detect([image], verbose=1)
-
-    # Visualize results
+    results = detector.detect([image], verbose=1)
     r = results[0]
-    #visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'], class_names, r['scores'])
     
     fig = plt.figure()
     ax1 = fig.add_subplot(1, 1, 1)
 
-    display_instances(image, r['rois'], r['masks'], r['class_ids'], class_names, r['scores'], ax=ax1)
+    mrcnn_viz.display_instances(image, r['rois'], r['masks'], r['class_ids'], class_names, r['scores'], ax=ax1)
     fig.tight_layout()
     plt.show(block=True)
-
-if __name__ == "__main__":
-    print(f"=======================>{matplotlib.get_backend()}")
-    detect()
-    print(f"=======================>{matplotlib.get_backend()}")
-
-
