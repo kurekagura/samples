f3d6f616f65f30c68e2b0853a187d835b031dff7
kurekagura <26186258+kurekagura@users.noreply.github.com>
2023-07-01 00:02:47 +0900

    Chart.jsテスト。

---
+++ Blazor/App1/Client/LibChart.cs
@@ -0,0 +1,23 @@
+﻿namespace App1.Client
+{
+    public class ChartJSON
+    {
+        public string type { get; set; }
+        public ChartData data { get; set; }
+    }
+
+    public class ChartData
+    {
+        public IEnumerable<string> labels { get; set; }
+        public IEnumerable<ChartDataset> datasets { get; set; }
+    }
+
+    public class ChartDataset
+    {
+        public string label { get; set; }
+        public IEnumerable<int> data { get; set; }
+        public string backgroundColor { get; set; }
+        public string borderColor { get; set; }
+        public int borderWidth { get; set; }
+    }
+}
